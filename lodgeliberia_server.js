c132b8076b7172a09c81b9d55537d1c6aa30436e
jamesgayflor <shekep1000@gmail.com>
2024-09-13 11:39:46 +0000

    updates

--- lodgeliberia_server.js
+++ lodgeliberia_server.js
@@ -52,6 +52,7 @@ function initializeDatabase() {
                 available_from DATE,
                 available_to DATE,
                 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                IMAGES BLOB NOT NULL,
                 FOREIGN KEY (user_id) REFERENCES users(id)
             )`
         ];
@@ -71,7 +72,52 @@ initializeDatabase();
 
 // 
 server.get("/", (req, res) => {
-    res.render("lodgeliberia_home");
+
+    // Pulling Places Information from database
+    lodge_liberia_db.all(`
+        SELECT 
+             users.fullname AS host_name,
+             host_listings.title AS property_title,
+             host_listings.description AS property_description,
+             host_listings.price_per_night AS property_price_per_night,
+             host_listings.Images AS images,
+             host_listings.available_from,
+             CASE strftime('%m', host_listings.available_from)
+                WHEN '01' THEN 'January'
+                WHEN '02' THEN 'February'
+                WHEN '03' THEN 'March'
+                WHEN '04' THEN 'April'
+                WHEN '05' THEN 'May'
+                WHEN '06' THEN 'June'
+                WHEN '07' THEN 'July'
+                WHEN '08' THEN 'August'
+                WHEN '09' THEN 'September'
+                WHEN '10' THEN 'October'
+                WHEN '11' THEN 'November'
+                WHEN '12' THEN 'December'
+            END AS available_month
+        FROM users
+        JOIN host_listings
+        ON users.id = host_listings.user_id
+        `, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        // Process each row to convert images to Base64
+        const host_listings = rows.map(row => ({
+            host_name: row.host_name,
+            property_title: row.property_title,
+            property_description: row.property_description,
+            property_price_per_night: row.property_price_per_night,
+            available_month: row.available_month,
+            // Convert BLOB to Base64 for each image
+            base64Image: row.images ? Buffer.from(row.images).toString('base64') : null
+        }));
+
+        // Pass the listings array to your EJS template
+        res.render('lodgeliberia_home', { host_listings });
+    });
+
 })
 
 // Port Application is listening on {Port: 5600}
---
+++ public/js/small_device_search_functionality.js
@@ -0,0 +1,8 @@
+// Variable Creation for hosting sm_mobile_search
+const mobile_search = document.getElementById("sm_mobile_search");
+
+
+// Functionality Implementation
+mobile_search.addEventListener('focus', () => {
+    console.log("Input focus");
+})
