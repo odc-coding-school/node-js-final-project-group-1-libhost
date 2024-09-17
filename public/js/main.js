6fe750ee9611939ba123f721fd3ff3ce78e30752
jamesgayflor <shekep1000@gmail.com>
2024-09-17 16:57:44 +0000

    updates

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-vote_btns = document.querySelectorAll(".vote_bn");
-console.log(vote_btns);
-
-vote_btns.forEach(element => {
-    element.addEventListener('click', () => {
-        window.location.replace("/vote_canditate");
-    })
-});
